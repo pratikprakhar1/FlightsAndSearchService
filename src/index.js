04dc41373e8efe59b078c10f2ba0903176a6fd91
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-08-19 23:10:13 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = async() =>{
+
+    //create the express object
+    const app = express();
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT}`);
+        //console.log(process.env);
+    }); 
+
+}
+
+setupAndStartServer();
