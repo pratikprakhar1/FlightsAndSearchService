41953d548aa8be2c99824cc0106b60db28c18b35
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-08-25 10:55:31 +0530

    Added get all cities api

--- src/controllers/city-controllers.js
+++ src/controllers/city-controllers.js
@@ -83,10 +83,31 @@ const update = async (req, res) => {
         });
     }
 }
+//getAll
+const getAll = async(req,res) =>{
+    try {
+        const citites = await cityService.getAllCities();
+        return res.status(200).json({
+            data: citites,
+            success: true,
+            message: 'Successfully fetched all cities',
+            err: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to get all the cities',
+            err: error
+        });
+    }
+}
 
 module.exports = {
     create,
     destroy,
     get,
-    update
+    update,
+    getAll
 }
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -45,9 +45,9 @@ class CityService {
         }
     }
 
-    async getAllCities(filter) {
+    async getAllCities() {
         try {
-            const cities = await this.cityRepository.getAllCities({name:filter.name});
+            const cities = await this.cityRepository.getAllCities();
             return cities;
         } catch (error) {
             console.log("Something went wrong at service layer");
